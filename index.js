bffdb77a31ee770192084c9737e4ee5000c6401d
Linah Muhonja <linahmuhonjaimbari@gmail.com>
2022-08-16 18:33:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+// currentUser = "Muhonja";
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0 , 1)} !`;
